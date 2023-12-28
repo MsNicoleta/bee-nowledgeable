51d0b34888611a860e071a60d7f5106d2e00f54f
Nicoleta Grubi <msnicoleta@outlook.com>
2023-12-28 21:00:10 +0000

    adding the file  structure &  Styling landing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
+import Bee1 from './img/Bee1.png';
+import Bee2 from './img/Bee2.png';
+import Flowers1 from './img/Flowers1.png';
+// import Flowers2 from './img/Flowers2.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <img src={Bee2} className="Bee-2" alt="bee" />
+          <img src={Bee1} className="Bee-1" alt="bee" />
+          <h1 className="App-title">
+            Bee Knowledgeable
+          </h1>
+          <h3 className="App-intro">Step into the world of bees.
+            <br />Learn all about these beautiful workers that help life flourish.</h3>
+          <div className="Flowers-container">
+            <img src={Flowers1} className="Flowers1" alt="Flowers-background" />
+            {/* <img src={Flowers2} className="Flowers2" alt="Flowers-background" /> */}
+          </div>
+        </div>
       </header>
     </div>
   );
