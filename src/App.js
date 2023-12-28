ca8c1fdcdacbb763b2bf133fc28bd9f66c6fa9e2
Nicoleta Grubi <msnicoleta@outlook.com>
2023-12-28 21:31:26 +0000

    first page styling completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Bee1 from './img/Bee1.png';
 import Bee2 from './img/Bee2.png';
 import Flowers1 from './img/Flowers1.png';
-// import Flowers2 from './img/Flowers2.png';
+import Flowers2 from './img/Flowers2.png';
 import './App.css';
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div>
+
           <img src={Bee2} className="Bee-2" alt="bee" />
           <img src={Bee1} className="Bee-1" alt="bee" />
           <h1 className="App-title">
@@ -16,10 +17,9 @@ function App() {
           </h1>
           <h3 className="App-intro">Step into the world of bees.
             <br />Learn all about these beautiful workers that help life flourish.</h3>
-          <div className="Flowers-container">
-            <img src={Flowers1} className="Flowers1" alt="Flowers-background" />
-            {/* <img src={Flowers2} className="Flowers2" alt="Flowers-background" /> */}
-          </div>
+          <img src={Flowers1} className="Flowers1" alt="Flowers-background" />
+          <img src={Flowers2} className="Flowers2" alt="Flowers-background" />
+
         </div>
       </header>
     </div>
