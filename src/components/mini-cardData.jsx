a10ff24650eddcf066c1008b1e8cf984f2b9c1c4
Nicoleta Grubi <msnicoleta@outlook.com>
2024-02-03 21:10:35 +0000

    The style for the cards to be completed #16

--- src/components/mini-cardData.jsx
+++ src/components/mini-cardData.jsx
@@ -7,32 +7,32 @@ const BeeDetails = [
   {
     img: Honeybees,
     title: 'Honeybees',
-    description: '🐝 Honeybees are social insects that live in colonies, with each colony consisting of a queen, worker bees, and drones.'
+    description: 'Honeybees are social insects that live in colonies, with each colony consisting of a queen, worker bees, and drones.'
   },
   {
     img: Bumblebees,
     title: 'Bumblebees',
-    description: '🐝 Bumblebees are robust, fuzzy bees known for their distinctive black and yellow stripes. Unlike honeybees, bumblebee colonies are typically smaller and do not store large quantities of honey.'
+    description: 'Bumblebees are robust, fuzzy bees known for their distinctive black and yellow stripes. '
   },
   {
     img: MiningBees,
     title: 'Mining Bees',
-    description: '🐝 Mining Bees are solitary bees known for their nesting behavior, where females dig tunnels in the ground to create individual nests.',
+    description: 'Mining Bees are solitary bees known for their nesting behavior, where females dig tunnels in the ground to create individual nests.',
   },
   {
     img: Honeybees,
     title: 'Honeybees',
-    description: '🐝 Honeybees are social insects that live in colonies, with each colony consisting of a queen, worker bees, and drones.',
+    description: 'Honeybees are social insects that live in colonies, with each colony consisting of a queen, worker bees, and drones.',
   },
   {
     img: Bumblebees,
     title: 'Bumblebees',
-    description: '🐝 Bumblebees are robust, fuzzy bees known for their distinctive black and yellow stripes. Unlike honeybees, bumblebee colonies are typically smaller and do not store large quantities of honey.',
+    description: 'Bumblebees are robust, fuzzy bees known for their distinctive black and yellow stripes. ',
   },
   {
     img: MiningBees,
     title: 'Mining Bees',
-    description: '🐝 Mining Bees are solitary bees known for their nesting behavior, where females dig tunnels in the ground to create individual nests.',
+    description: 'Mining Bees are solitary bees known for their nesting behavior, where females dig tunnels in the ground to create individual nests.',
   },
 
 
